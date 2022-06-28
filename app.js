0347603bf6e24186650c4f3497ca45d262e08883
gitman00 <atomicdev09@gmail.com>
2022-06-28 19:25:14 +0545

    Added About Section

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ let menuToggler = document.querySelector('.menuToggler');
 let span = document.querySelector('.menuToggler span');
 let nav = document.querySelector('nav');
 let header = document.querySelector('header');
-let cursor = document.querySelector('.cursor');
-let caption = document.querySelector('.caption');
+
+
 menuToggler.addEventListener("click", () => {
 	nav.classList.toggle('active');
 	span.classList.toggle('active');
 	header.classList.toggle('active');
 });
 
-// caption.addEventListener('mousemove' , (e) => {
-// 	cursor.style.left  = e.pageX + 'px';
-// 	cursor.style.top  = e.pageY + 'px';
-// })
+
+document.addEventListener('scroll', () => {
+	let title = document.querySelector('.title2 h1');
+
+})
